c74b499af1402791fca72f3ba3e55b7675c49f4b
HappyPlanning <48041555+taymila@users.noreply.github.com>
2022-06-12 22:24:16 +0800

    update

---
+++ Responses.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def sample_responses(input_text):
+    user_message = str(input_text).lower()
+
+    if user_message in ("hello", "hi"):
+        return "Hi, how may I help today?"
+
+    if user_message in ("what are you", "who are you?"):
+        return "I am ChargeEV Bot, a Bot created to provide you with dedicated support"
+
+    if user_message in ("time", "time now"):
+        now = datetime.now()
+        date_time = now.strftime("%d/$m, %H:%M")
+
+        return str(date_time)
+
+    return "Could you rephrase that?"
