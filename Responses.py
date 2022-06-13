873b8776a996941ddfe85225dd3e0e2dfceaa1ef
TayMilaJK <taymila1@gmail.com>
2022-06-13 23:50:16 +0800

    first commit

--- Responses.py
+++ Responses.py
@@ -4,7 +4,7 @@ def sample_responses(input_text):
     user_message = str(input_text).lower()
 
     if user_message in ("hello", "hi"):
-        return "Hi, how may I help today \n here are some options:?"
+        return "Hi, how may I help today \n here are some options:"
 
     if user_message in ("what are you", "who are you?"):
         return "I am ChargeEV Bot, a Bot created to provide you with dedicated support"
